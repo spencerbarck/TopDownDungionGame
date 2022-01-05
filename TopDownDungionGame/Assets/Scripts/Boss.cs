35d6d3016e1e16bbcfc8c24c86a873017ef4fb0d
spencerbarck <60077342+spencerbarck@users.noreply.github.com>
2022-01-04 21:01:08 -0600

    Refactored Boss Class

--- TopDownDungionGame/Assets/Scripts/Boss.cs
+++ TopDownDungionGame/Assets/Scripts/Boss.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class Boss : Enemy
 {
-    public float[] minionsSpeed = {3f, -3f};
-    public float minionDistance = 0.25f;
-    public Transform[] minions;
-
+    [SerializeField]
+    private float[] minionsSpeed = {3f, -3f};
+    [SerializeField]
+    private float minionDistance = 0.25f;
+    [SerializeField]
+    private Transform[] minions;
     private void Update()
     {
         for (int i = 0; i < minions.Length; i++)
         {
+            // Minions rotate around boss
             minions[i].position = transform.position + new Vector3(-Mathf.Cos(Time.time * minionsSpeed[i]) * minionDistance, Mathf.Sin(Time.time * minionsSpeed[i])* minionDistance, 0);
         }
-        
-
     }
-
-
 }
